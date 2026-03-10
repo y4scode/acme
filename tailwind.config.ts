180c560ec865c1b67d8206e83b0ceab8609d5f20
Yasmim Paiva de Araujo <123039698+y4scode@users.noreply.github.com>
2026-03-10 12:33:34 +0000

    chore: configurar framework css com processador

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+    content: [
+        './src/app/**/*.{js,jsx,ts,tsx}',
+        '.src/components/**/*.{js,jsx,ts,tsx}'
+    ],
+    theme: {
+        extend: {}
+    },
+    plugins: []
+};
+
+export default config; []
