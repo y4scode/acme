b670514214ff5ae61f2caeb47a3c3816683f31e9
Yasmim Paiva de Araujo <123039698+y4scode@users.noreply.github.com>
2026-03-10 13:13:41 +0000

    style: adicionar estilizacao temporaria

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type {Metadata} from 'next';
 import {inter} from './ui/fonts';
-import '@/app/globals.css';
+import './globals.css';
 
 export const metadata: Metadata = {
     title: 'ACME'
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Page(){
-    return 'Sistema em construção.';
+    return (
+        <main className="bg-black h-screen flex justify-center items-center">
+            <h1 className="text-amber-50 font-semibold text-4xl">Sistema em construção.</h1>
+        </main>
+        );
 };
