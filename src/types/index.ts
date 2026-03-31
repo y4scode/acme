0c83d2babe48808a89c49d8edd63d10630ecac49
Yasmim Paiva de Araujo <123039698+y4scode@users.noreply.github.com>
2026-03-31 12:49:22 +0000

    feat: definir contrato de tipos para usuarios, clientes, faturas, receitas e api

---
+++ src/types/index.ts
@@ -0,0 +1,47 @@
+export interface User{
+    id: string;
+    name: string;
+    email: string;
+    password: string
+};
+
+export type CreateUserData = Omit<User, 'id'>;
+export type UpdateUserData = Partial<CreateUserData>;
+
+export interface Customer{
+    id: string;
+    name: string;
+    email: string;
+    imageUrl: string
+};
+
+export type CreateCustomerData = Omit<Customer, 'id'>;
+export type UpdateCustomerData = Partial<CreateCustomerData>;
+
+export type InvoiceStatus = 'PENDENTE' | 'PAGO';
+
+export interface Invoice{
+    id: string;
+    customer_id: string;
+    amount: number;
+    date: Date;
+    status: InvoiceStatus;
+};
+
+export type CreateInvoiceData = Omit<Invoice, 'id'>;
+export type UpdateInvoiceData = Partial<CreateInvoiceData>
+
+export interface Revenue {
+    month: string;
+    revenue: number;
+};
+
+export interface ApiResponse<T> {
+    data: T;
+    message?: string;
+};
+
+export interface ApiError {
+    error: string;
+    details?: unknown;
+};
